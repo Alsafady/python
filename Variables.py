fcb4fdc109f5d23c2ea898ab1a72c20fd4e1e4c0
Abdulaziz Alsafadi <iweb1700@hotmail.com>
2018-08-18 23:18:18 +0300

    lists

--- Variables.py
+++ Variables.py
@@ -1,2 +1,39 @@
 myint = 7
 print(myint)
+
+myfloat = 7.0
+print(myfloat)
+
+myfloat = float(7)
+print(myfloat)
+
+mystring = 'hello'
+print(mystring)
+
+mystring = "hello"
+print(mystring)
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a, b = 3, 4
+print(a,b)
+
+
+# Mixing operators between numbers and strings is not supported:
+# This will not work!
+one = 1
+two = 2
+hello = "hello"
+
+print(one + two + hello)
