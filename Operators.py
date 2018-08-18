a214b2b7cd271b5815b6f21225d9583109be3af4
Abdulaziz Alsafadi <iweb1700@hotmail.com>
2018-08-18 23:45:45 +0300

    op

--- Operators.py
+++ Operators.py
@@ -17,3 +17,17 @@ print(cubed)
 
 helloworld = "hello" + " " + "world"
 print(helloworld)
+
+
+LotsOfHellos = "Hello" * 10
+print(LotsOfHellos)
+
+
+#Using Operators with Lists (arrays)
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+
+print([1,2,3] * 3)
