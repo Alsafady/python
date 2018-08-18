4b841a54cff83b0f08d2b8db37e6a03a904ec2a9
Abdulaziz Alsafadi <iweb1700@hotmail.com>
2018-08-18 23:38:53 +0300

    Operators

---
+++ Operators.py
@@ -0,0 +1,19 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+
+
+#Exponentiation الأس
+squared = 7 ** 2
+cubed = 2 ** 3
+
+print(squared)
+print(cubed)
+#====================
+
+
+helloworld = "hello" + " " + "world"
+print(helloworld)
